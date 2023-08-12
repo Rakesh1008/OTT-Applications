c9a8ca5117746b77ec1f147c88fc166e7eed8da9
rakesh <rakeshkumar.panda@logituit.com>
2023-08-13 00:26:43 +0530

    initial setup for project

---
+++ movies-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       movies
+      </header>
+    </div>
+  );
+}
+
+export default App;
